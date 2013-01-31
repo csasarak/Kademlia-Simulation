ea65d77557081017dd5d814c2fec97674224dd1d
Christopher Sasarak <chris.sasarak@gmail.com>
2013-01-31 14:05:54 -0500

    Initial commit of files

---
+++ KConstants.py
@@ -0,0 +1,5 @@
+# Author: Christopher Sasarak
+
+# This file contains constants for use by the Kademlia simulation
+
+bit_string_size = 8
---
+++ Node.py
@@ -0,0 +1,9 @@
+# Author: Christopher Sasarak
+import KConstants
+
+# Implementation of a node object
+class Node(object):
+    
+    def __new__(self, ID=None):
+        pass
+    
