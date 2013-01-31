7d3eb7c13bbef4ab86d6305e27f745fa18029538
Christopher Sasarak <chris.sasarak@gmail.com>
2013-01-31 14:22:54 -0500

    Added KBucket stubs and a kBucket list to the Node object

--- KademliaConstants.py
+++ KademliaConstants.py
@@ -3,3 +3,4 @@
 # This file contains constants for use by the Kademlia simulation
 
 bit_string_size = 8
+k_bucket_size = 20
--- Node.py
+++ Node.py
@@ -4,6 +4,24 @@ import KademliaConstants
 # Implementation of a node object
 class Node(object):
     
-    def __new__(self, ID=None):
-        pass
+    def __new__(self, id):
+        self.id = ID
+
+        self.kBuckets = list()
+
+        # Populate the Node with empty buckets
+        for i in range(0, KademliaConstants.bit_string_size):
+            self.kBuckets = KBucket(KademliaConstants.k_bucket_size)
+
     
+    
+
+class KBucket(object):
+
+    def __new__(self, k = None):
+
+        # Use default k size if none is provided
+        if(k == None):
+            self.k = KConstants.k_bucket_size
+
+        self.bucket_list = list()
