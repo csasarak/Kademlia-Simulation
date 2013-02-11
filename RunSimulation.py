2a1780ac07464674a3428e7502021175f4e5d35a
cms5347 <cms5347@rit.edu>
2013-02-11 15:39:47 -0500

    Made the RunSimulation readouts easier to read

--- RunSimulation.py
+++ RunSimulation.py
@@ -6,32 +6,44 @@ import sys
 import node
 from simulation import Simulation
 
-if len(sys.argv) < 5:
-    print """Usage: ./RunSimulation.py <random seed> <network size> <trials> <disable frequency>
+usage = """Usage: ./RunSimulation.py <random seed> <network size> <trials> <disable frequency>
 During each trial, the disable frequency is multiplied by the trial number to get the number of nodes to disable for that trial"""
+
+if len(sys.argv) < 5:
+    print usage
     sys.exit(1)
 
-network_size = int(sys.argv[2])
-trials = int(sys.argv[3])
-seed = int(sys.argv[1])
-lookups = 1000
+try:
+    network_size = int(sys.argv[2])
+    trials = int(sys.argv[3])
+    seed = int(sys.argv[1])
+    # The rate at which we will disable nodes 
+    node_disable_n = int(sys.argv[4])
+except ValueError:
+    print usage
+    sys.exit(1)
 
-# The rate at which we will disable nodes 
-node_disable_n = int(sys.argv[4])
+print "Building network"    
 sim = Simulation(seed, network_size)
-
-
+lookups = 1000
 # Train the network a bit by doing random lookups
 for i in range(3000):
+    if i % 10 == 0:
+        sys.stdout.write("\rInitializing network {0:.1f}%".format(i / 3000.0 * 100))
+        sys.stdout.flush()
     sim.perform_node_lookup()
+    
+sys.stdout.write("\rInitializing network 100% \n\n")
+sys.stdout.flush()
 
 for n in sim.nodes:
     n.totalLookupDuration = 0
-    
-start_state = sim.rand.getstate()    
-for trial in range(trials):
 
-    print "Trial ", trial, " with ", trial * node_disable_n, " nodes disabled"
+for trial in range(trials):
+    print "================================================================================"
+    sim.disable_nodes(trial * node_disable_n)
+    
+    print "\nTrial ", trial, " with ", trial * node_disable_n, " nodes disabled"
 
     # Perform lookups and then get the average time
     for i in range(lookups):
@@ -42,8 +54,4 @@ for trial in range(trials):
         time_sum = time_sum + n.totalLookupDuration
         n.totalLookupDuration = 0
 
-    sim.disable_nodes(trial * node_disable_n)
-
-    print "\t Avg. lookup time: ", (time_sum / network_size/ 1000)
-    # Reset the generator to the original state
-    # sim.rand.setstate(start_state)
+    print "Avg. lookup time: {0:.4f} seconds".format(time_sum / network_size/ 1000)
