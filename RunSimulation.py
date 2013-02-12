4a58062f35e4d15b43409b676ba2056fbac075e8
cms5347 <cms5347@rit.edu>
2013-02-11 21:57:11 -0500

    Updated so that data is output to a csv file which is usable by gnuplot, fixed an error with the node disable rate increasing too quickly

--- RunSimulation.py
+++ RunSimulation.py
@@ -32,11 +32,11 @@ print "Building network"
 sim = Simulation(seed, network_size)
 lookups = 1000
 # Train the network a bit by doing random lookups
-for i in range(network_size / 2):
-    if i % 20 == 0:
-        sys.stdout.write("\rInitializing network {0:.1f}%".format(i / (network_size / 2.0) * 100))
-        sys.stdout.flush()
-    sim.perform_node_lookup()
+# for i in range(int(network_size / 2)):
+#     if i % 20 == 0:
+#         sys.stdout.write("\rInitializing network {0:.1f}%".format(i / (network_size / 2.0) * 100))
+#         sys.stdout.flush()
+#     sim.perform_node_lookup()
     
 sys.stdout.write("\rInitializing network 100% \n\n")
 sys.stdout.flush()
@@ -45,13 +45,13 @@ for n in sim.nodes:
     n.totalLookupDuration = 0
 
 with open(out_filename, 'wb') as csv_file:
-    csv_writer = csv.writer(csv_file, delimiter=' ')
-    csv_writer.writerow(["# Trial", "Disabled Nodes", "Avg. lookup time"])
+    csv_writer = csv.writer(csv_file, delimiter=' ', quotechar='#', quoting=csv.QUOTE_MINIMAL)
+    csv_writer.writerow(["# Trial", "Disabled Nodes Rate {}".format(node_disable_n), "Avg. lookup time"])
     
     for trial in range(trials):
         print "================================================================================"
         disabled_nodes = trial * node_disable_n
-        sim.disable_nodes(disabled_nodes)
+        sim.disable_nodes(node_disable_n)
     
         print "\nTrial ", trial + 1, " with ", disabled_nodes, " nodes disabled"
 
@@ -60,10 +60,13 @@ with open(out_filename, 'wb') as csv_file:
             sim.perform_node_lookup()
 
         time_sum = 0
+        node_count = 0 # The number of nodes that had lookups performed on them
         for n in sim.nodes:
-            time_sum = time_sum + n.totalLookupDuration
-            n.totalLookupDuration = 0
+            if n.totalLookupDuration != 0: 
+                time_sum = time_sum + n.totalLookupDuration
+                node_count = node_count + 1
+                n.totalLookupDuration = 0
 
-        avg_lookup_time = time_sum / network_size/ 1000
+        avg_lookup_time = time_sum / node_count / 1000
         print "Avg. lookup time: {0:.4f} seconds".format(avg_lookup_time)
-        csv_writer.writerow([trial, disabled_nodes, avg_lookup_time])
+        csv_writer.writerow([trial + 1, avg_lookup_time])
